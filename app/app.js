1508b1916b22626292ba724d03b343adaca9ba84
fnpratto <fnpratto@gmail.com>
2024-09-18 16:03:29 -0300

    agregando endpoints, documentacion de instalacion

---
+++ app/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+client.connect();
+
+//ENDPOINT PING
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+
+// API FREE DIRECRIONARY
+app.get('/dictionary', async (req, res) => {
+    const word = req.query.word;
+    const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const { phonetics, meanings } = response.data[0];
+    res.json({ phonetics, meanings });
+  });
+  
+///spaceflight_news:
+app.get('/spaceflight_news', async (req, res) => {
+    const response = await axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=5');
+    const titles = response.data.map(article => article.title);
+    res.json(titles);
+});
+
+
+//
+app.get('/quote', async (req, res) => {
+    const response = await axios.get('https://api.quotable.io/random');
+    const { content, author } = response.data;
+    res.json({ content, author });
+  });
+  
---
+++ documentacion|notas.md
@@ -0,0 +1,17 @@
+*Instalar:*
+sudo apt install npm
+*Express:*
+npm install express
+*Docker* (chequear instalacion)
+docker --version
+docker-compose --version
+*Configurar Redis*
+npm install redis
+*Instalacion para  Endpoints
+npm install axios (Instala axios para hacer requests a la API de Free Dictionary)
+
+Notas 
+- Cree docker file
+- yml parece que ya viene completo, falta revisar
+- *Configurar Nginx* Nota: edite basico para ver como funciona nginx_reverse_proxy, buscar mejor*
+- Hay que revisar carpetas/ direcciones/ puertos / links (yml)
