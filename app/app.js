e99efc6b144bf96bd6f9e569c39795f0a7c1c045
fnpratto <fnpratto@gmail.com>
2024-09-21 00:31:35 -0300

    Ajustes de Docker

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
-const express = require('express');
+import express from 'express';
+import redis from 'redis';
+
 const app = express();
 const port = 3000;
 
-
-
-const redis = require('redis');
 const client = redis.createClient();
 
 client.on('error', (err) => console.log('Redis Client Error', err));
@@ -23,25 +22,25 @@ app.listen(port, () => {
 
 
 // API FREE DIRECRIONARY
-app.get('/dictionary', async (req, res) => {
-    const word = req.query.word;
-    const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-    const { phonetics, meanings } = response.data[0];
-    res.json({ phonetics, meanings });
-  });
+// app.get('/dictionary', async (req, res) => {
+//     const word = req.query.word;
+//     const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+//     const { phonetics, meanings } = response.data[0];
+//     res.json({ phonetics, meanings });
+//   });
   
-///spaceflight_news:
-app.get('/spaceflight_news', async (req, res) => {
-    const response = await axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=5');
-    const titles = response.data.map(article => article.title);
-    res.json(titles);
-});
+// ///spaceflight_news:
+// app.get('/spaceflight_news', async (req, res) => {
+//     const response = await axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=5');
+//     const titles = response.data.map(article => article.title);
+//     res.json(titles);
+// });
 
 
-//
-app.get('/quote', async (req, res) => {
-    const response = await axios.get('https://api.quotable.io/random');
-    const { content, author } = response.data;
-    res.json({ content, author });
-  });
+// //
+// app.get('/quote', async (req, res) => {
+//     const response = await axios.get('https://api.quotable.io/random');
+//     const { content, author } = response.data;
+//     res.json({ content, author });
+//   });
   
